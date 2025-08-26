7ba0c0056900a9045cca1675274fa4201b76a68a
Nikolaus Knop <niko.knop003@gmail.com>
2025-08-26 12:59:57 +0200

    Add audio preview mechanism and improve the layout

--- script.js
+++ script.js
@@ -32,3 +32,49 @@ function copyCode(button) {
         setTimeout(() => (button.textContent = "Copy"), 1500);
     });
 }
+
+function isValidEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+
+let cachedEmail = null
+
+async function requestFullTrack(btn, trackName) {
+    const emailField = btn.previousElementSibling;
+    if (btn.textContent == "Download full track") {
+        console.log("Making ", emailField, "visible");
+        emailField.style.display = "block";
+        if (cachedEmail != null) {
+            emailField.value = cachedEmail;
+        }
+        btn.textContent = "Confirm email"
+        let previewDisclaimer = btn.nextElementSibling
+        previewDisclaimer.style.display = "none"
+    } else if (btn.textContent == "Confirm email") {
+        let statusText = emailField.previousElementSibling;
+        if (!isValidEmail(emailField.value)) {
+            alert("Invalid email")
+            return
+        }
+        cachedEmail = emailField.value
+
+        let data = new FormData()
+        data.append("email", emailField.value)
+        data.append("message", trackName)
+        let response = await fetch("https://formspree.io/f/mqadkbqw", {
+            method: "POST",
+            body: data,
+            headers: { 'Accept': 'application/json' }
+        });
+
+        if (response.ok) {
+            statusText.style.display = "block"
+            emailField.value = ""
+            emailField.style.display = "none"
+            btn.style.display = "none"
+        } else {
+            alert("There was a problem submitting your form.")
+        }
+    }
+}
